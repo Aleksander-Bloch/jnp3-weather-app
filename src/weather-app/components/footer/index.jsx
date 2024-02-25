2e2736046809dd4e9912d9d92059f47bd66270e4
Aleksander Bloch <alekbloch21@gmail.com>
2024-02-25 22:03:14 +0100

    Modify anchor to open github page in new tab

--- src/weather-app/components/footer/index.jsx
+++ src/weather-app/components/footer/index.jsx
@@ -5,7 +5,7 @@ import { FooterWrapper } from "./FooterWrapper.jsx";
 export const Footer = () => {
   return (
     <FooterWrapper>
-      <a href="https://github.com/Aleksander-Bloch"><FaGithub/></a>
+      <a href="https://github.com/Aleksander-Bloch/jnp3-weather-app" target="_blank" rel="noreferrer"><FaGithub/></a>
       <ThemeSwitcher/>
     </FooterWrapper>
   )
